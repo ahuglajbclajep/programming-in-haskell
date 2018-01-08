e629de14b04245d9146c56db9bd73db912916fcc
ahuglajbclajep <ahuglajbclajep@gmail.com>
2018-01-08 21:59:06 +0900

    use tasty-discover

--- test/Section1Test.hs
+++ test/Section1Test.hs
@@ -1,12 +1,11 @@
+module Section1Test where
+
 import Test.Tasty
 import Test.Tasty.HUnit
 import Section1
 
-main :: IO ()
-main = defaultMain tests
-
-tests :: TestTree
-tests = testGroup "Unit tests"
+test_UnitTests :: [TestTree]
+test_UnitTests =
     [ testCase "1.7.3" $
         product' [2, 3, 4] @?= 24
     ]
