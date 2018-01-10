e50d48366b6dbc8c5c5a08420833d37c68cf4367
ahuglajbclajep <ahuglajbclajep@gmail.com>
2018-01-11 01:16:17 +0900

    down resolver for ghc-mod

--- src/Section1/Exercise3.hs
+++ src/Section1/Exercise3.hs
@@ -3,5 +3,5 @@ module Section1.Exercise3
     ) where
 
 product' :: Num a => [a] -> a
-product' [] = 1
+product' []     = 1
 product' (x:xs) = x * product' xs
--- src/Section1/Exercise4.hs
+++ src/Section1/Exercise4.hs
@@ -3,7 +3,7 @@ module Section1.Exercise4
     ) where
 
 qsort' :: Ord a => [a] -> [a]
-qsort' []       = []
+qsort' []     = []
 qsort' (x:xs) = qsort' larger ++ [x] ++ qsort' smaller
   where
     smaller = [a | a <- xs, a <= x]
