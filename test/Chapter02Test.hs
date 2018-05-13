098ce9f7999cbc8edb5e026ce1026bcb025c17b4
ahuglajbclajep <ahuglajbclajep@gmail.com>
2018-05-13 18:54:26 +0900

    format by brittany

--- test/Chapter02Test.hs
+++ test/Chapter02Test.hs
@@ -1,27 +1,23 @@
 module Chapter02Test where
 
-import Test.Tasty
-import Test.Tasty.HUnit
-import Chapter02.Exercise3
-import Chapter02.Exercise4
-import Chapter02.Exercise5
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Chapter02.Exercise3
+import           Chapter02.Exercise4
+import           Chapter02.Exercise5
 
 test_Ch02UnitTests :: [TestTree]
 test_Ch02UnitTests =
-    [ testCase "Ex3" $
-        n @?= 2
-    , testGroup "Ex4"
-        [ testCase "last'" $
-            last' [1..5] @?= 5
-        , testCase "last''" $
-            last'' [1..5] @?= 5
-        , testCase "last'''" $
-            last''' [1..5] @?= 5
+    [ testCase "Ex3" $ n @?= 2
+    , testGroup
+        "Ex4"
+        [ testCase "last'" $ last' [1 .. 5] @?= 5
+        , testCase "last''" $ last'' [1 .. 5] @?= 5
+        , testCase "last'''" $ last''' [1 .. 5] @?= 5
         ]
-    , testGroup "Ex5"
-        [ testCase "init'" $
-            init' [1..5] @?= [1..4]
-        , testCase "init''" $
-            init' [1..5] @?= [1..4]
+    , testGroup
+        "Ex5"
+        [ testCase "init'" $ init' [1 .. 5] @?= [1 .. 4]
+        , testCase "init''" $ init' [1 .. 5] @?= [1 .. 4]
         ]
     ]
--- test/Chapter04Test.hs
+++ test/Chapter04Test.hs
@@ -1,23 +1,19 @@
 module Chapter04Test where
 
-import Test.Tasty
-import Test.Tasty.HUnit
-import Chapter04.Exercise1
-import Chapter04.Exercise2
-import Chapter04.Exercise6
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Chapter04.Exercise1
+import           Chapter04.Exercise2
+import           Chapter04.Exercise6
 
 test_Ch04UnitTests :: [TestTree]
 test_Ch04UnitTests =
-    [ testCase "Ex1" $
-        halve [1..6] @?= ([1..3], [4..6])
-    , testGroup "Ex2"
-        [ testCase "safeTail" $
-            safeTail [1..5] @?= [2..5]
-        , testCase "safeTail'" $
-            safeTail' [1..5] @?= [2..5]
-        , testCase "safeTail''" $
-            safeTail'' [1..5] @?= [2..5]
+    [ testCase "Ex1" $ halve [1 .. 6] @?= ([1 .. 3], [4 .. 6])
+    , testGroup
+        "Ex2"
+        [ testCase "safeTail" $ safeTail [1 .. 5] @?= [2 .. 5]
+        , testCase "safeTail'" $ safeTail' [1 .. 5] @?= [2 .. 5]
+        , testCase "safeTail''" $ safeTail'' [1 .. 5] @?= [2 .. 5]
         ]
-    , testCase "Ex6" $
-        mult 1 2 3 @?= 6
+    , testCase "Ex6" $ mult 1 2 3 @?= 6
     ]
